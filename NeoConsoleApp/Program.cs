96392943c17bd1c3764db7f958207e83567c1e18
Kirill Shirokov <ksshirokov01@gmail.com>
2024-04-01 19:07:33 +0200

    Add Neo console app

---
+++ NeoConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace NeoConsoleApp
+{
+    internal class Program
+    {
+
+        public static void PrepareConsole()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+
+        public static void ClearConsole()
+        {
+            Console.Clear();
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+        }
+
+        public static void PrintToConsole(string message)
+        {
+            Console.WriteLine(message);
+            // wait for user to read a message
+            Thread.Sleep(2000);
+        }
+
+        static void Main(string[] args)
+        {
+            PrepareConsole();
+
+            PrintToConsole("Wake up, Neo...");
+            ClearConsole();
+
+            PrintToConsole("The matrix has you...");
+            ClearConsole();
+
+            PrintToConsole("Follow the white rabbit.");
+            ClearConsole();
+
+            PrintToConsole("Knock, knock, Neo.");
+            ClearConsole();
+        }
+    }
+}
